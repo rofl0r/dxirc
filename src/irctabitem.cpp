8e912077c7454e1b9e1af050c8e25c318412610d
david.vachulka <david.vachulka@gmail.com>
2009-06-14 17:54:41 +0000

    malinka pro makovku
    git-svn-id: http://dxirc.googlecode.com/svn/trunk@97 ad0a5998-b8b9-11dd-9d36-1d0c09c95ed7

--- src/irctabitem.cpp
+++ src/irctabitem.cpp
@@ -2452,7 +2452,7 @@ FXint IrcTabItem::LaunchLink(const FXString &link)
 #ifdef WIN32
     return ((FXint)ShellExecuteA(NULL,"open",FXPath::enquote(link).text(),NULL,NULL,SW_NORMAL)) > 32;
 #else
-    static const char * browsers[]={/*"xdg-open",*/"firefox","konqueror","opera","netscape","dillo",NULL};
+    static const char * browsers[]={/*"xdg-open",*/"firefox","konqueror","opera","netscape","dillo","safari",NULL};
     FXString path = FXSystem::getExecPath();
     FXString exec;
     for(int i=0; browsers[i]!=NULL; i++)
